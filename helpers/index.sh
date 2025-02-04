af1239b6f7a21003cf65935a156c560ba1a4576c
Chris Pinnock <chris_pinnock@mac.com>
2025-02-04 15:57:57 +0100

    fix

--- helpers/index.sh
+++ helpers/index.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
+# New
+TARGETS="$TARGETS debian-12 debian-12-arm64 ubuntu-2204-lts rocky-linux-9"
+[ -f "platforms" ] && TARGETS=$(cat platforms)
+
 cd Sources/pkgbeta-tzinit-org
 cp ../../web/index.html .
 
-# New
-TARGETS="$TARGETS debian-12 debian-12-arm64 ubuntu-2204-lts rocky-linux-9"
 
 if [ ! -z "$1" ]; then
 
 	echo "===> Building index for $1"
 
 	for i in ${TARGETS}; do
+		echo "==> $i"
 		mkdir -p $i/$1
 		mkdir -p $i/$1/dev
 		(cd $i/$1 && mks3idx > index.html)
@@ -20,6 +23,7 @@ fi
 
 echo "===> Building index"
 for i in ${TARGETS}; do
+	echo "==> $i"
 	(cd $i && mks3idx > index.html)
 	(cd $i/dev && mks3idx > index.html)
 done
